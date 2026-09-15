31f6e9c0b3eba439845df92deab81023800eee0e
rohandatey@gmail.com <rohandatey@gmail.com>
2026-09-15 11:55:02 +0530

    delete button added

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -16,15 +16,25 @@ const App = () => {
     console.log(mainTask);
   };
 
+  // for deletetask
+  const deleteHandler = (i)=>{
+    let copytask = [...mainTask]
+    copytask.splice(i,1)
+    setMainTask(copytask)
+}
+
   let showtask = <h2>No task now</h2>;
-  if (mainTask > 0) {
+  if (mainTask.length > 0) {
     showtask = mainTask.map((t, i) => {
       return (
-        <li>
-          <div key={i} className="flex justify-between mb-5">
+        <li className="flex items-center justify-between">
+          <div key={i} className="flex justify-between mb-5 w-2/3">
             <h5 className="text-2xl font-semibold">{t.title}</h5>
             <h6 className="text-xl font-semibold">{t.desc}</h6>
           </div>
+          <button onClick={()=>{
+            deleteHandler(i)
+          }} className="bg-red-500 text-white px-4 py-2 rounded-md">Delete</button>
         </li>
       );
     });
@@ -58,8 +68,7 @@ const App = () => {
             // console.log(e.target.value);
             // value maine title ko store kiya and setTitle se change hoga
             setDesc(e.target.value);
-          }}
-        />
+          }}/>
 
         <button className="px-2 py-2 bg-black text-white font-bold text-xl rounded cursor-pointer">
           Add Task
